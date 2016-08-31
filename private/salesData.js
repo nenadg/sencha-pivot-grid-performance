d9dece80b47aaeff1dd4913b1c039e7645ead100
Nenad Golubovic <nenad@propelmedia.com>
2016-08-31 16:53:00 +0200

    - cleanup

--- private/salesData.js
+++ private/salesData.js
@@ -1,34 +1,9 @@
 module.exports = function(limit){
-    /*,
-            { 
-                'id': 2, 
-                'company': 'Brocorp',
-                'country': 'US',
-                'person': 'Rajko',
-                'date': '2015-07-07',
-                'value': 202,
-                'quantity': 301,
-                'year': '2017',
-                'month': '07',
-                'continent': 'America'
-            },
-            { 
-                'id': 3, 
-                'company': 'Brocorp',
-                'country': 'US',
-                'person': 'Bosko',
-                'date': '2015-07-07',
-                'value': 3322,
-                'quantity': 301,
-                'year': '2017',
-                'month': '07',
-                'continent': 'America'
-            }
-        ] */
-    //};
+
     var getrnd = function(){
         return Math.random().toString(36).substring(3)
     }
+
     var i = 0, ret = [];
 
     limit = limit ? limit : 10;
@@ -54,6 +29,5 @@ module.exports = function(limit){
         i++;
     }
 
-    console.log('don')
     return ret;
 };
